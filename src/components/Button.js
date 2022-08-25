a1b2f75a5621959a5fb5552597a074a32e35e969
prasannakarki77 <prasanna.karki77@gmail.com>
2022-08-25 12:38:27 +0545

    added button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,44 @@
 import React, { useEffect, useState } from "react";
 import "../styles/button.scss";
 const Button = (props) => {
-  const [label, setLabel] = useState("default");
-
+  const [label, setLabel] = useState("Default");
+  const [variant, setVariant] = useState("");
+  const [disabled, setDisabled] = useState("");
+  const [disableShadow, setdisableShadow] = useState("");
+  const [size, setSize] = useState("");
+  const [color, setColor] = useState("default");
   useEffect(() => {
     if (props.label !== undefined) {
       setLabel(props.label);
     }
+    if (props.variant !== undefined) {
+      setVariant(props.variant);
+    }
+    if (props.disabled) {
+      setDisabled("disabled");
+    }
+    if (props.disableShadow) {
+      setdisableShadow("disableShadow");
+    }
+    if (props.size !== undefined) {
+      setSize(props.size);
+    }
+    if (props.color !== undefined) {
+      setColor(props.color);
+    }
   }, []);
   return (
     <div className="button-container">
+      <h1>{props.disabled}</h1>
+      <p>{`<Button ${
+        props.variant !== undefined ? `variant="${props.variant}"` : ""
+      }  ${props.size !== undefined ? `size="${props.size}"` : ""}  ${
+        props.color !== undefined ? `color="${props.color}"` : ""
+      } ${props.disabled ? "disabled" : ""} ${
+        props.disableShadow ? "disableShadow" : ""
+      }/>`}</p>
       <button
-        className={`button button__${props.variant} button__state--${props.state}`}
+        className={`button button__${variant} button__size--${size} button__color--${color} button__state--${disabled} button__state--${disableShadow}`}
       >
         {label}
       </button>
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,9 +4,16 @@ import "../styles/button.scss";
 const Buttons = () => {
   return (
     <div className="buttons-container">
-      <Button label="Button1" />
-      <Button label="Button2" />
       <Button />
+      <Button variant="outline" />
+      <Button variant="text" />
+      <Button disableShadow />
+      <Button variant="text" disabled />
+      <Button size="lg" color="primary" />
+      <Button size="md" color="primary" />
+      <Button size="sm" color="primary" />
+      <Button color="secondary" />
+      <Button color="danger" />
     </div>
   );
 };
